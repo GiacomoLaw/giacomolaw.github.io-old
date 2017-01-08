b2717c43810f0dc81c67e261705ff808e9744f10
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-01-08 16:34:57 +0000

    added rotation alert

---
+++ js/rotation.js
@@ -0,0 +1,3 @@
+if(window.innerHeight > window.innerWidth){
+    alert("I've noticed your on mobile! This page isn't currrently mobile friendly - please rotate to landscape. I'm working on fixing it!");
+}
