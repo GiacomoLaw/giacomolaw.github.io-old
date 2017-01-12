0c2a2cb1d7a6b139debe4c43d3bda5ae7c6d14e0
Giacomo Lawrance <GiacomoLaw@users.noreply.github.com>
2017-01-12 18:08:41 +0000

    redirect to mobile site

--- js/rotation.js
+++ js/rotation.js
@@ -1,3 +1,3 @@
 if(window.innerHeight > window.innerWidth){
-    alert("I've noticed you're on mobile! This page isn't currrently mobile friendly - please rotate to landscape. I'm working on fixing it!");
+    window.location = "https://giacomolaw.github.io/mobile.html";
 }
